01909c690126f65ec57e3f0fc7b865b6057c978a
Princeton Baretto <princebaretto@gmail.com>
2020-04-16 11:25:52 +0530

    added dates also

--- Flask API/oneto15.py
+++ Flask API/oneto15.py
@@ -10,10 +10,11 @@ for i in range(len(training_set)):
 
 training_set = data.iloc[:600, 0:1]
 new_dataset = training_set.reset_index()
-new_dataset = new_dataset.drop('date', axis = 1)
+# new_dataset = new_dataset.drop('date', axis = 1)
 made_dataset = new_dataset.drop(indices)
 
 final_dataset = made_dataset.reset_index()
 main_dataset = final_dataset.drop('index',axis=1)
 
-print(len(main_dataset))
+dates = main_dataset.iloc[:,0].tolist()
+stocks = main_dataset.iloc[:,1].tolist()
